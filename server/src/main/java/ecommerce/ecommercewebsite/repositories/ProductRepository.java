b55dbf8275996b106f7a6a444e26fa11ba6e90f9
wojo321 <wojtek.f.99@gmail.com>
2022-09-13 00:22:53 +0200

    Added another way to handle cross origin policy

--- server/src/main/java/ecommerce/ecommercewebsite/repositories/ProductRepository.java
+++ server/src/main/java/ecommerce/ecommercewebsite/repositories/ProductRepository.java
@@ -4,5 +4,6 @@ import ecommerce.ecommercewebsite.model.product.Product;
 import org.springframework.stereotype.Repository;
 
 @Repository
+//Another way to handle cross origin problems -> @CrossOrigin("url address")
 public interface ProductRepository extends AbstractRepository<Product, Long> {
 }
